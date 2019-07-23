fd630f0adedfede3ab3e1ca938070cfb441a7f60
Naresh Vadala <naresh.vadala@questionmark.com>
2019-07-23 18:54:02 +0530

    Directory Info example

---
+++ Csharp70-483/C4-ImplementDataAccess/DirectoryExamples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace C4_ImplementDataAccess
+{
+    public class DirectoryExamples
+    {
+        public void CreateDirectoryUsingDirectoryInfo()
+        {
+            var path = @"c:\Naresh\CsharpTemp";
+            var directoryInfo = new DirectoryInfo(path);
+            directoryInfo.Create();
+
+        }
+    }
+}
